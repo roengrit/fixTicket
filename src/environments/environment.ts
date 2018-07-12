1cc6a67c14fc7ef0a51240d9123a15eec8d0ab98
roengrit@smlsoft.com <logon.firstclass@gmail.com>
2018-07-12 09:25:19 +0700

    angular firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDv6htNozvUW9O8fJZJfHwHb7hahwEjaws",
+    authDomain: "fixticket-cd8b4.firebaseapp.com",
+    databaseURL: "https://fixticket-cd8b4.firebaseio.com",
+    projectId: "fixticket-cd8b4",
+    storageBucket: "fixticket-cd8b4.appspot.com",
+    messagingSenderId: "1079142151917"
+  } 
 };
